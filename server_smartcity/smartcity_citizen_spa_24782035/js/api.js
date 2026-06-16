ed0cc63c6149394d8aacd21af4ee86339a42c157
Akbar Dzikrullah <akbardzikrullah20@gmail.com>
2026-06-16 14:40:31 +0000

    di api.js ganti ke port publik nginx 8004

--- server_smartcity/smartcity_citizen_spa_24782035/js/api.js
+++ server_smartcity/smartcity_citizen_spa_24782035/js/api.js
@@ -1,5 +1,5 @@
 // Sesuaikan dengan URL server Django kamu
-const BASE_URL = "http://103.151.63.85:8000";
+const BASE_URL = "http://103.151.63.85:8004";
 
 async function requestAPI(endpoint, method = 'GET', bodyData = null) {
     // 1. Ambil access_token dari penyimpanan lokal browser
